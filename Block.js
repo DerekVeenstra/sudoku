45cfdef56371f0f8cbdf6b5ac61ce11da6393140
Derek Veenstra <derekveenstra@Dereks-MacBook-Pro.local>
2022-11-18 15:31:49 -0500

    Strategy testing

--- Block.js
+++ Block.js
@@ -1,12 +1,27 @@
+const defs = require('./defs');
+
 class Block {
 
     rows = [];
     cols = [];
+    cells = [];
 
-    constructor(rows, cols) {
+    constructor(rows, cols, game) {
         this.rows = rows;
         this.cols = cols;
-    };
+
+        this.setCellValues(game);
+    }
+
+    setCellValues(game) {
+        for(let i = 0; i < this.rows.length; i++) {
+            for(let j = j < this.cols.length; j++) {
+                this.cells.push(game[i][j]);
+            }
+        }
+
+        console.log('this.cells', this.cells);
+    }
 }
 
 module.exports = Block;
--- GameParser.js
+++ GameParser.js
@@ -62,17 +62,17 @@ module.exports = {
     parseBlocks : function(rows, cols) {
 
         return [
-            new Block([0, 1, 2], [0, 1, 2]),
-            new Block([0, 1, 2], [3, 4, 5]),
-            new Block([0, 1, 2], [6, 7, 8]),
+            new Block([0, 1, 2], [0, 1, 2], game),
+            new Block([0, 1, 2], [3, 4, 5], game),
+            new Block([0, 1, 2], [6, 7, 8], game),
+            
+            new Block([3, 4, 5], [0, 1, 2], game),
+            new Block([3, 4, 5], [3, 4, 5], game),
+            new Block([3, 4, 5], [6, 7, 8], game),
 
-            new Block([3, 4, 5], [0, 1, 2]),
-            new Block([3, 4, 5], [3, 4, 5]),
-            new Block([3, 4, 5], [6, 7, 8]),
-
-            new Block([6, 7, 8], [0, 1, 2]),
-            new Block([6, 7, 8], [3, 4, 5]),
-            new Block([6, 7, 8], [6, 7, 8]),
+            new Block([6, 7, 8], [0, 1, 2], game),
+            new Block([6, 7, 8], [3, 4, 5], game),
+            new Block([6, 7, 8], [6, 7, 8], game),
         ];
     }
 }
