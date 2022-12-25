82bd78cb5cd23a3f94f14d6275ad83a068dd23fd
Derek Veenstra <derekveenstra@Dereks-MacBook-Pro.local>
2022-12-25 09:16:28 -0500

    fixed cell allocation for blocks

--- Block.js
+++ Block.js
@@ -1,12 +1,15 @@
+const _ = require('lodash');
 const defs = require('./defs');
 
 class Block {
 
+    id;
     rows = [];
     cols = [];
     cells = [];
 
-    constructor(rows, cols, cells) {
+    constructor(id, rows, cols, cells) {
+        this.id = id;
         this.rows = rows;
         this.cols = cols;
 
@@ -14,11 +17,19 @@ class Block {
     }
 
     setCellValues(cells) {
-        for(let i = 0; i < this.rows.length; i++) {
-            for(let j = 0; j < this.cols.length; j++) {
-                this.cells.push(cells[i][j]);
-            }
-        }
+        console.log('rows', this.rows);
+        console.log('cols', this.cols);
+        _.forEach(this.rows, rowIndex => {
+            _.forEach(this.cols, colIndex => {
+                this.cells.push(cells[rowIndex][colIndex]);
+            });
+        });
+
+        console.log('test', this.id, this.cells);
+    }
+
+    isEqualToBlock(block) {
+        return _.isEqual(block.rows, this.rows) && _.isEqual(block.cols, this.cols);
     }
 }
 
